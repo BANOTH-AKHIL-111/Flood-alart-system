cbd2e3defd35de97df4dd75e7d416b8acab04726
Banoth Akhil <akhilbanoth111@gmail.com>
2026-01-21 12:31:37 +0530

    new

--- backend/db.py
+++ backend/db.py
@@ -3,5 +3,9 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_PUBLIC_URL")
 
-conn = psycopg2.connect(DATABASE_URL)
+conn = psycopg2.connect(
+    DATABASE_URL,
+    sslmode="require"
+)
+
 cursor = conn.cursor()
